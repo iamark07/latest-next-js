87723e9352f767fa1b66d2d43992bee9cfb6df8c
Arbaz Karimi <arbazkarimi@gmail.com>
2024-10-09 11:36:30 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,75 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import React, { useState } from "react";
+
+function page() {
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
+  const [mainTask, setMainTask] = useState([]);
+
+  const submit_handler = (e)=>{
+    e.preventDefault();
+    setMainTask([...mainTask, { title , desc }]);
+    settitle("");
+    setdesc("");
+    // console.log(mainTask);
+  }
+
+  const deleteHandeler = (i) =>{
+    let copytask = [...mainTask];
+    copytask.splice(i,1);
+    setMainTask(copytask);
+  }
+
+  let renderTask = <h1 className="bg-zinc-300 p-5 text-black rounded">No Task Available</h1>
+
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t , i) =>{
+      return(
+        <li key={i} className="bg-zinc-300 p-5 text-black rounded flex justify-between items-center">
+          <h2 className="text-2xl font-semibold">{t.title}</h2>
+          <p className="text-2xl font-medium">{t.desc}</p>
+          <button className="px-4 py-2 bg-red-400 text-white text-lg" onClick={()=>{
+            deleteHandeler(i)
+          }}>Delete</button>
+        </li>
+      )
+    })
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="todo-list bg-black p-5 md:p-10">
+      <h1 className="text-white text-3xl font-bold text-center">
+        Arbaz Todo List
+      </h1>
+      <form onSubmit={submit_handler} className="grid grid-cols-2 gap-5 w-full md:w-3/4 m-auto my-10">
+        <input
+          type="text"
+          placeholder="Enter Task"
+          value={title}
+          className="rounded px-5 py-3 border border-white outline-none text-white bg-transparent col-span-2 md:col-span-1"
+          onChange={(e)=>{
+            settitle(e.target.value);
+          }}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <input
+          type="text"
+          placeholder="Description"
+          value={desc}
+          className="rounded px-5 py-3 border border-white outline-none text-white bg-transparent col-span-2 md:col-span-1"
+          onChange={(e)=>{
+            setdesc(e.target.value);
+          }}
+        />
+        <button className="rounded bg-green-500 text-white px-5 py-3 col-span-2">
+          Add Task
+        </button>
+      </form>
+      <hr />
+      <ul className="flex gap-5 flex-col m-auto mt-10 w-full md:w-3/4">
+        {renderTask}
+      </ul>
     </div>
   );
 }
+
+export default page;
